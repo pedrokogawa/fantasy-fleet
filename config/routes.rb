c5be056ca01130355631561472bbbf382d22fa9d
Shingie Taira <tairashingie@gmail.com>
2024-07-13 16:59:49 +0900

    added resources on bookings

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :vehicles
+  resources :bookings, only: [:index, :show, :edit,:update, :new, :create ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_071054) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_13_071530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_day"
+    t.date "end_date"
+    t.integer "total_price"
+    t.integer "status"
+    t.bigint "user_id", null: false
+    t.bigint "vehicle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["vehicle_id"], name: "index_bookings_on_vehicle_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,5 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_071054) do
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "vehicles"
   add_foreign_key "vehicles", "users"
 end
