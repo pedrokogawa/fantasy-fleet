3937a3edb034cb99177af649edafa61bf2889c84
Shingie Taira <tairashingie@gmail.com>
2024-07-15 19:51:13 +0900

    fixed name of start date

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_071530) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_104549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.date "start_day"
+    t.date "start_date"
     t.date "end_date"
     t.integer "total_price"
     t.integer "status"
