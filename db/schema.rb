dce81a3c5b039363317dbafe8ee78b2558a85aea
Pedro Kogawa <pedrokogawa@gmail.com>
2024-07-16 13:54:50 +0900

    vehicle validation corrected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_071530) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_043925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_071530) do
     t.integer "price_per_day"
     t.string "description"
     t.string "location"
-    t.string "availability"
+    t.integer "availability"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
