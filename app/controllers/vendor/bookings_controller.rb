1b10681c8c89fa75bdcbaee3786a3ecf9fd8a92d
Shingie Taira <tairashingie@gmail.com>
2024-07-16 19:48:02 +0900

    added index page routes and views

--- app/controllers/vendor/bookings_controller.rb
+++ app/controllers/vendor/bookings_controller.rb
@@ -1,5 +1,9 @@
 class Vendor::BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update]
+
   def index
+    @vendor = current_user.id
+    @bookings = Booking.all.where(id: @vendor)
   end
 
   def show
@@ -9,5 +13,15 @@ class Vendor::BookingsController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price, :status, :vehicle_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_043925) do
     t.date "start_date"
     t.date "end_date"
     t.integer "total_price"
-    t.integer "status", default: 1
+    t.integer "status"
     t.bigint "user_id", null: false
     t.bigint "vehicle_id", null: false
     t.datetime "created_at", null: false
