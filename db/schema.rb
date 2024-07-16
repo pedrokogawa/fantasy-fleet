255e4d9a6ce585931db825e0fff8c32922a2cf86
Pedro Kogawa <pedrokogawa@gmail.com>
2024-07-16 16:44:34 +0900

    chaged layout of new vehicle page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_104549) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_043925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_104549) do
     t.date "start_date"
     t.date "end_date"
     t.integer "total_price"
-    t.integer "status"
+    t.integer "status", default: 1
     t.bigint "user_id", null: false
     t.bigint "vehicle_id", null: false
     t.datetime "created_at", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_104549) do
     t.integer "price_per_day"
     t.string "description"
     t.string "location"
-    t.string "availability"
+    t.integer "availability"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
