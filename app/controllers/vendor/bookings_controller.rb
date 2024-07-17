6518ecdf96106f9c64cda5a62a0f70399817e976
Shingie Taira <tairashingie@gmail.com>
2024-07-17 17:20:09 +0900

    added accept controller action

--- app/controllers/vendor/bookings_controller.rb
+++ app/controllers/vendor/bookings_controller.rb
@@ -6,6 +6,11 @@ class Vendor::BookingsController < ApplicationController
     @vehicles = Vehicle.all.where(user_id: @vendor_id)
     @bookings = Booking.all.where(vehicle_id: @vehicles.pluck(:id))
   end
+  
+  def accept
+    @booking.accepted!
+    redirect_to vendor_bookings_path, notice: "Booking was successfully accepted!"
+  end
 
   def show
   end
