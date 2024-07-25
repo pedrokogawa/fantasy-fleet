366e1eea796de443331f280b0f8ca636b386f2bb
Shingie Taira <tairashingie@gmail.com>
2024-07-26 01:03:54 +0900

    disabled dates that are already booked

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,20 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 export default class extends Controller {
-  static values = {
-    disabledDates: Array
-  }
   connect() {
+    const datesJson = this.element.dataset.dates; // GET values from the form
+    const disabledDates = JSON.parse(datesJson || "[]"); // PARSE to JSON
+
     flatpickr(this.element, {
       altInput: true,
       altFormat: "F j, Y",
       dateFormat: "Y-m-d",
       minDate: "today",
       maxDate: new Date().fp_incr(365),
-      disable: this.disabledDatesValue.map(date => ({
-        from: date.from,
-        to: date.to
-      }))
+      disable: disabledDates.flatMap(dateRange => {
+        return {
+          from: dateRange.from,
+          to: dateRange.to
+        };
+      })
     })
   }
 }
