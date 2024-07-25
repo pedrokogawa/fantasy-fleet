96469a05293407ebacfe16487a207f98f02b7296
Shingie Taira <tairashingie@gmail.com>
2024-07-25 23:30:03 +0900

    modified the FlatPickr JavaScript to handle start and end dates

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 export default class extends Controller {
+  static values = {
+    disabledDates: Array
+  }
   connect() {
     flatpickr(this.element, {
       altInput: true,
       altFormat: "F j, Y",
       dateFormat: "Y-m-d",
       minDate: "today",
-      maxDate: new Date().fp_incr(365)
+      maxDate: new Date().fp_incr(365),
+      disable: this.disabledDatesValue.map(date => ({
+        from: date.from,
+        to: date.to
+      }))
     })
   }
 }
