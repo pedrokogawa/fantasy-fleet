31f920b58cf5c2c1c53381097c85793b1ba79831
Shingie Taira <tairashingie@gmail.com>
2024-07-27 09:28:11 +0900

    created vehicle policy

--- app/policies/vehicle_policy.rb
+++ app/policies/vehicle_policy.rb
@@ -7,8 +7,32 @@ class VehiclePolicy < ApplicationPolicy
 
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
   end
 end
